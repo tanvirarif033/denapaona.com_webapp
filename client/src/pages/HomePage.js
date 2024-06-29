56f54a41ca7253be6a815cb0e917ca8a94f7bee9
Amirul026 <mominamirul96@gmail.com>
2024-06-30 00:33:38 +0600

    v1

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,16 +1,73 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
-import { useAuth } from '../context/auth'
-const HomePage = () => {
-    const [auth,setAuth]=useAuth()
-  return (
-    <Layout  title={"Best Offers"}>
-    <h1>HomePage</h1>
-    <pre>{JSON.stringify(auth,null,4)}</pre>
-    </Layout>
-      
-    
-  )
-}
+import React, { useState, useEffect } from "react";
+import Layout from "../components/Layout/Layout";
+import { useAuth } from "../context/auth";
+import axios from "axios";
+import { Checkbox } from "antd";
 
-export default HomePage
+const HomePage = () => {
+  const [auth, setAuth] = useAuth();
+  const [products, setProduct] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  //get all cat
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data?.success) {
+        setCategories(data?.categories);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
+  //get products
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/product/get-product");
+      setProduct(data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+  return (
+    <Layout title={"Best Offers"}>
+      <div className="row mt-3">
+        <div className="col-md-3">
+          <h4 className="text-cener">Filter By Category</h4>
+          {categories?.map((c) => (
+            <Checkbox key={c._id} onChange={(e) => console.log(e)}></Checkbox>
+          ))}
+        </div>
+        <div className="col-md-9">
+          <h1 className="text-center">All product</h1>
+          <div className="d-flex flex-wrap">
+            {products?.map((p) => (
+              <div className="card m-2" style={{ width: "18rem" }}>
+                <img
+                  src={`/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">{p.description}</p>
+                  <button class="btn btn-primary ms-1">More Details</button>
+                  <button class="btn btn-secondary ms-1">ADD TO CART</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default HomePage;
