2617fe68a08c929a15a85a1502c88b5809f24e2c
tanvirarif033 <tanvirarif0802@gmail.com>
2024-07-02 12:22:55 +0600

    product page

---
+++ client/src/pages/ProductDetails.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const ProductDetails = () => {
+  return (
+    <Layout>
+        <h1>Product Details</h1>
+
+    </Layout>
+  )
+}
+
+export default ProductDetails
