d8169426465885179991f8a101c3c422fced40e3
Amirul026 <mominamirul96@gmail.com>
2024-07-05 21:19:30 +0600

    product-show

--- client/src/pages/CategoryProduct.js
+++ client/src/pages/CategoryProduct.js
@@ -26,13 +26,47 @@ const CategoryProduct = () => {
 
   return (
     <Layout>
-        <div className="container mt-3">
-          <h1>?????</h1>
-            <h1 className="text-center">{category?.name}</h1>
+      <div className="container mt-3">
+        <h4 className="text-center">Category - {category?.name}</h4>
+        <h6 className="text-center">{products?.length} result found</h6>
+        <div className="row">
+          <div className="col-md-9 offset-1">
+            <div className="d-flex flex-wrap">
+              {products?.map((p) => (
+                <div
+                  className="card m-2"
+                  style={{ width: "18rem" }}
+                  key={p._id}
+                >
+                  <img
+                    src={`https://denapaona-com-webapp-server.vercel.app/api/v1/product/product-photo/${p._id}`}
+                    className="card-img-top"
+                    alt={p.name}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{p.name}</h5>
+                    <p className="card-text">
+                      {p.description.substring(0, 20)}...
+                    </p>
+                    <p className="card-text">$ {p.price}</p>
+                    <button
+                      className="btn btn-primary ms-1"
+                      onClick={() => navigate(`/product/${p.slug}`)}
+                    >
+                      More Details
+                    </button>
+                    <button className="btn btn-secondary ms-1">
+                      ADD TO CART
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-        
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default CategoryProduct
+export default CategoryProduct;
