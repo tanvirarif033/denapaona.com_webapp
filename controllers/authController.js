9823d110f6e8d6072046a89165c6e7bad1ce1a9e
Amirul026 <mominamirul96@gmail.com>
2024-07-11 21:39:17 +0600

    create-product loader

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,10 @@
 import userModel from "../models/userModel.js";
- import { comparePassword, hashPassword } from "./../helpers/authHelper.js";
- import JWT from "jsonwebtoken";
+import { comparePassword, hashPassword } from "./../helpers/authHelper.js";
+import JWT from "jsonwebtoken";
 
 export const registerController = async (req, res) => {
   try {
-    const { name, email, password, phone, address ,answer} = req.body;
+    const { name, email, password, phone, address, answer } = req.body;
     //validations
     if (!name) {
       return res.send({ message: "Name is Required" });
@@ -42,7 +42,7 @@ export const registerController = async (req, res) => {
       phone,
       address,
       password: hashedPassword,
-      answer
+      answer,
     }).save();
 
     res.status(201).send({
@@ -62,72 +62,68 @@ export const registerController = async (req, res) => {
 
 //POST LOGIN
 export const loginController = async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      //validation
-      if (!email || !password) {
-        return res.status(404).send({
-          success: false,
-          message: "Invalid email or password",
-        });
-      }
-      //check user
-      const user = await userModel.findOne({ email });
-      if (!user) {
-        return res.status(404).send({
-          success: false,
-          message: "Email is not registerd",
-        });
-      }
-      const match = await comparePassword(password, user.password);
-      if (!match) {
-        return res.status(200).send({
-          success: false,
-          message: "Invalid Password",
-        });
-      }
-      //token
-      const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: "7d",
-      });
-      res.status(200).send({
-        success: true,
-        message: "login successfully",
-        user: {
-          _id: user._id,
-          name: user.name,
-          email: user.email,
-          phone: user.phone,
-          adddress: user.address,
-          role : user.role,
-        },
-        token,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({
+  try {
+    const { email, password } = req.body;
+    //validation
+    if (!email || !password) {
+      return res.status(404).send({
         success: false,
-        message: "Error in login",
-        error,
+        message: "Invalid email or password",
       });
     }
-  };
-
-
-
-  
-
-  //test controller
-export const testController = (req, res) => {
-    try {
-      res.send("Protected Routes");
-    } catch (error) {
-      console.log(error);
-      res.send({ error });
+    //check user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Email is not registerd",
+      });
     }
-  };
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+    //token
+    const token = JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).send({
+      success: true,
+      message: "login successfully",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        adddress: user.address,
+        role: user.role,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in login",
+      error,
+    });
+  }
+};
 
-  //forgotPasswordController
+//test controller
+export const testController = (req, res) => {
+  try {
+    res.send("Protected Routes");
+  } catch (error) {
+    console.log(error);
+    res.send({ error });
+  }
+};
+
+//forgotPasswordController
 
 export const forgotPasswordController = async (req, res) => {
   try {
