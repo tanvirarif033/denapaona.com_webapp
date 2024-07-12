5b2c93f7cfb11ff72b698d42d76b75efd2d7e0e2
Amirul026 <150332448+Amirul026@users.noreply.github.com>
2024-07-12 23:55:22 +0600

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -118,7 +118,7 @@ export const loginController = async (req, res) => {
         name: user.name,
         email: user.email,
         phone: user.phone,
-        adddress: user.address,
+        address: user.address,
         role: user.role,
       },
       token,
