499c36fae0a8f99134841474d8d9479935e19b0b
Amirul026 <mominamirul96@gmail.com>
2024-07-12 21:48:37 +0600

    form show

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./../../components/Layout/Layout";
 import UsersMenu from "../../components/Layout/UsersMenu";
+import { useAuth } from "../../context/auth";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Profile = () => {
+  //context
+  const [auth, setAuth] = useAuth();
+  //state
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        "https://denapaona-com-webapp-server.vercel.app/api/v1/auth/register",
+        {
+          name,
+          email,
+          password,
+          phone,
+          address,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
   return (
     <Layout title={"Your Profile"}>
       <div className="container-fluid m-3 p-3">
@@ -11,7 +43,65 @@ const Profile = () => {
             <UsersMenu />
           </div>
           <div className="col-md-9">
-            <h1>Your Profile</h1>
+            <div className="form-container">
+              <form onSubmit={handleSubmit}>
+                <h4 className="title">USER PROFILE</h4>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="form-control"
+                    placeholder="Enter Your Name"
+                    required
+                    autoFocus
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-control"
+                    placeholder="Enter Your Email "
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="form-control"
+                    placeholder="Enter Your Password"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="number"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    className="form-control"
+                    placeholder="Enter Your Phone"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    className="form-control"
+                    placeholder="Enter Your Address"
+                    required
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  UPDATE
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
