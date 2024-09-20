4097335cc35a9de41659d5c698555683fe48ca5d
tanvirarif033 <tanvirarif0802@gmail.com>
2024-09-21 02:25:27 +0600

    categories

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -1,17 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import useCategory from "../hooks/useCategory";
 import Layout from "../components/Layout/Layout";
+import "../styles/Categories.css";
 const Categories = () => {
   const categories = useCategory();
+
   return (
     <Layout title={"All Categories"}>
       <div className="container">
         <div className="row">
           {categories.map((c) => (
-            <div className="col-md-6 mt-5 mb-3 gx-3 gy-3" key={c._id}>
-              <Link to={`/category/${c.slug}`} className="btn btn-primary">
-                {c.name}
+            <div className="col-md-4 mt-5 mb-3" key={c._id}>
+              <Link to={`/category/${c.slug}`} className="category-link">
+                <div className="card category-card">
+                  <img
+                    src={`https://denapaona-com-webapp-server.vercel.app/api/v1/category/photo/${c._id}`}
+                    className="card-img-top"
+                    alt={c.name}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{c.name}</h5>
+                  </div>
+                </div>
               </Link>
             </div>
           ))}
